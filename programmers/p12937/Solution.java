606ff2e4cadad04e45a03889fa8fd203fec8d008
송형근 <soo1095@naver.com>
2024-07-30 11:33:32 +0900

    240730

---
+++ programmers/p12937/Solution.java
@@ -0,0 +1,11 @@
+package programmers.p12937;
+
+class Solution {
+    public static String solution(int num) {
+        if(num%2==0){ // 짝수
+            return "Even";
+        }else {
+            return "Odd";
+        }
+    }
+}
